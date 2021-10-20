b2b45c3ac6ae7703a1ee94a7a1fdb07181b1d88f
seungchae <id6639@naver.com>
2021-10-20 23:20:15 +0900

    first

--- class_01/counter.js
+++ class_01/counter.js
@@ -6,9 +6,9 @@ function count() {
   counter++;
   document.querySelector('h1').innerHTML = counter;
 
-  if (counter % 10 === 0) {
-    alert(`Count is now ${counter}`);
-  }
+  // if (counter % 10 === 0) {
+  //   alert(`Count is now ${counter}`);
+  // }
 }
 
 document.addEventListener('DOMContentLoaded', function () {
