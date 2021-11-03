75249f6d3d14257708d586f55594081864f52e9a
seungchae <id6639@naver.com>
2021-11-04 00:39:48 +0900

    2th

--- class_02/Algorithms/SumAlgorithm.js
+++ class_02/Algorithms/SumAlgorithm.js
@@ -1,29 +1,39 @@
 // 합계 알고리즘(Sum Algorithm): 주어진 범위에 주어진 조건에 해당하는 값(Value)들의 합계.
 
-const a = 1;
-const b = 2;
-const c = 3;
+//[1] Input(입력)
 
-console.log(a + b);
-console.log(a + c);
-console.log(b + c);
+//[2] Process(처리)
 
-var x;
+//[3] Output(출력)
 
-x = 10;
-console.log(x); //10
 
-x += 5;
-console.log(x); //15
 
-x -= 5;
-console.log(x); //10
 
-x *= 5;
-console.log(x); //50
 
-x /= 5;
-console.log(x); //10
+// const a = 1;
+// const b = 2;
+// const c = 3;
 
-x %= 5;
-console.log(x); //0
+// console.log(a + b);
+// console.log(a + c);
+// console.log(b + c);
+
+// var x;
+
+// x = 10;
+// console.log(x); //10
+
+// x += 5;
+// console.log(x); //15
+
+// x -= 5;
+// console.log(x); //10
+
+// x *= 5;
+// console.log(x); //50
+
+// x /= 5;
+// console.log(x); //10
+
+// x %= 5;
+// console.log(x); //0
