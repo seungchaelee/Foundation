b249395e14c6475c6641f3ea8257d6efd6fabb2b
seungchae <id6639@naver.com>
2021-11-04 17:50:16 +0900

    2th

--- class_02/Algorithms/ArithmeticSequence.js
+++ class_02/Algorithms/ArithmeticSequence.js
@@ -1,1 +1,19 @@
-// 등차수열(Arithmetic Sequence)
+// 등차수열(Arithmetic Sequence): 연속하는 두 수의 차익사 일정한 수열
+
+//[문제] 1부터 20까지의 정수 중 홀수의 합을 구하시오
+
+//[1] Input(입력)
+
+let sum = 0; //Sum
+
+//[2] Process(처리)
+
+for (let i = 0; i <= 20; i++) { //주어진 범위
+  if (i % 2 ==1) { //주어진 조건: 필터링(홀수) !(i % 2 != 1)
+    sum += i; //Sum
+  }
+}
+
+//[3] Output(출력)
+
+console.log('1부터 20까지의 홀수의 합:' + sum); //100
