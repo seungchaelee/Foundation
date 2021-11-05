2f4e6b67255534560fe17a3a24b72e5ea9361dd1
seungchae <id6639@naver.com>
2021-11-06 00:28:48 +0900

    2th

--- class_02/Algorithms/AverageAlgorithm.js
+++ class_02/Algorithms/AverageAlgorithm.js
@@ -1,1 +1,18 @@
 // 평균 알고리즘(Average Algorithm): 주어진 범위에 주어진 조건에 해당하는 값(Value)들의 평균
+
+//[문제] n명의 점수 중에서 80점 이상 95점 이하인 점수의 평균
+
+function Average() {
+
+  //[1] Input(입력)
+
+
+
+  //[2] Process(처리)
+
+
+  
+  //[3] Output(출력)
+}
+
+Average();
