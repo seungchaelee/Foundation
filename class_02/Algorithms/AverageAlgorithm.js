6fc879c3ffdeff578b5e4df1a0bd4e9c6d96a5af
seungchae <id6639@naver.com>
2021-11-06 01:05:34 +0900

    2th

--- class_02/Algorithms/AverageAlgorithm.js
+++ class_02/Algorithms/AverageAlgorithm.js
@@ -4,15 +4,30 @@
 
 function Average() {
 
-  //[1] Input(입력)
+  //[1] Input(입력): n명의 성적
 
-
+  let data = [90, 65, 78, 50, 95];
+  let sum = 0;  // 합계 담는 그릇
+  let count = 0;  // 개수 담는 그릇
+  let avg = 0.0;
 
   //[2] Process(처리)
 
+  for (let i = 0; i <= data.length; i++) {
+    if (data[i] >= 80 && data[i] <=95) {
+      sum += data[i]; // SUM
+      count++;  // COUNT
+    }
+  }
 
-  
+  if (sum != 0 && count !=0) {
+    avg = sum / count;  // AVERAGE
+  }
+
   //[3] Output(출력)
+
+  console.log('80점 이상 95점 이하인 자료의 평균: ' + avg);
+
 }
 
 Average();
