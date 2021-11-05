10aedbc4aa6e784dce008862acf707fc8b7c975d
seungchae <id6639@naver.com>
2021-11-06 01:23:38 +0900

    2th

---
+++ class_02/Algorithms/MaxAlgorithm.js
@@ -0,0 +1,23 @@
+// 쵀대값 알고리즘(Max Algorithm): 주어진 범위에 주어진 조건에 자료들중 가장 큰 값(Value)
+
+//[문제] 주어진 데이터 중에서 가장 큰 값(Valus)
+
+function Max() {
+
+  //[0] Initialize(초기화)
+
+
+
+  //[1] Input(입력)
+
+
+
+  //[2] Process(처리)
+
+
+
+  //[3] Output(출력)
+
+}
+
+Max();
