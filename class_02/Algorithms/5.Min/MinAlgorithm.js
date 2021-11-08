174edda8ed19439504a8fde8b2caa275a37b3491
seungchae <id6639@naver.com>
2021-11-09 00:17:55 +0900

    2th

--- class_02/Algorithms/5.Min/MinAlgorithm.js
+++ class_02/Algorithms/5.Min/MinAlgorithm.js
@@ -1,22 +1,22 @@
 // 최솟값 알고리즘(Min Algorithm): 주어진 범위에 주어진 조건에 자료들 중 가장 작은 값(Value)
 
-//[문제] 주어진 데이터 중에서 가장 작은 값(Valus)
+//[문제] 주어진 데이터 중에서 가장 작은 [짝수] 값(Valus)
 
 function Min() {
 
   //[0] Initialize(초기화)
 
-  let min = Number.MAX_SAFE_INTEGER
+  let min = Number.MAX_SAFE_INTEGER // 숫자 형식의 데이터 중 가장 큰 값으로 초기화
 
   //[1] Input(입력)
 
-  let numbers = [0, 1, 2, 3, 4, -5];
+  let numbers = [0, -3, -2, 3, 4, 7];  // Min: -3 -> Min: -2(짝수)
 
   //[2] Process(처리): Min
 
   for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] < min) {
-      min = numbers[i];
+    if (numbers[i] < min && numbers[i] % 2 ==0) { // 찍수 최솟값
+      min = numbers[i]; // Min: 더 작은 값으로 할당
     }
   }
 
