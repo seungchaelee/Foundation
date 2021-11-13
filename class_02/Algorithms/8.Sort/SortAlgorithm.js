5dea4b8b27783b385dfa357818475ce2528b544b
seungchae <id6639@naver.com>
2021-11-13 22:55:00 +0900

    2th

--- class_02/Algorithms/8.Sort/SortAlgorithm.js
+++ class_02/Algorithms/8.Sort/SortAlgorithm.js
@@ -1,22 +1,18 @@
-// 정렬 알고리즘(Sort Algorithm): 
+// 정렬 알고리즘(Sort Algorithm): 가장 [큰/작은] 데이터를 왼쪽으로 순서대로 이동
 
 //[문제]
 
 function Sort() {
 
-  //[0]
+  //[1] Input(입력)
 
 
 
-  //[1]
+  //[2] Process(처리): Selection Sort(선택 정렬)
 
 
 
-  //[2]
-
-
-
-  //[3]
+  //[3] Output(출력)
 
   console.log();
 
