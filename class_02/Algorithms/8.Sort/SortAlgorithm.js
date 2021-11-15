a437005095acd4f4dc1f43bcf52cb852c942a6a6
seungchae <id6639@naver.com>
2021-11-15 18:59:06 +0900

    2th

--- class_02/Algorithms/8.Sort/SortAlgorithm.js
+++ class_02/Algorithms/8.Sort/SortAlgorithm.js
@@ -7,6 +7,7 @@ function Sort() {
   //[1] Input(입력): Data Structure(Array, List, Stack, Queue, Tree, DB, ...)
 
   let data = [3, 2, 1, 5, 4]; // 정렬되지 않은 데이터
+  let N = data.length;
 
   //[2] Process(처리): Selection Sort(선택 정렬)
 
@@ -16,7 +17,7 @@ function Sort() {
 
   //[3] Output(출력): UI(Console, Desktop, Web, Mobile, ...)
 
-  for (let i = 0; i < data.length; i++) {
+  for (let i = 0; i < N; i++) {
     console.log(data[i]);
   }
 
