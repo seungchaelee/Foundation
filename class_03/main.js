9094bbb442da48eaf6a256785164270673c69adb
seungchae <id6639@naver.com>
2021-12-07 23:24:17 +0900

    3th

--- class_03/main.js
+++ class_03/main.js
@@ -16,3 +16,38 @@ let globalName = 'Global Name';
 }
 console.log(name);
 console.log(globalName);
+
+// var (don't ever use this!)
+// var hoisting (move declaration from bottom to top)
+// has no block scope
+{
+  age = 4;
+  var age;
+}
+console.log(age);
+
+// 3. Constants
+// favor immutable data type always for a few reasons:
+// - security
+// - thread safety
+// - reduce human mistakes
+const daysInweek = 7;
+const maxNumber = 5;
+
+// 4. Variable types
+// primitive, single item: number, string, boolean, null, undefiend, symbol
+// object, box container
+// function, first-class function
+
+const count = 17; // integer
+const size = 17.1;  // decimal number
+console.log(`value: ${count}, type: ${typeof count}`);
+console.log(`value: ${size}, type: ${typeof size}`);
+
+// number - special numeric values: infinity, -infinity, NaN
+const infinity = 1 / 0;
+const negativeInfinity = -1 / 0;
+const nAn = 'not a number' / 2;
+console.log(infinity);
+console.log(negativeInfinity);
+console.log(nAn);
