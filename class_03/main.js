59481842b09ad5f4b4791edbb8ea57f20d51f1d6
seungchae <id6639@naver.com>
2021-12-07 21:02:29 +0900

    3th

---
+++ class_03/main.js
@@ -0,0 +1,12 @@
+// 1. Use strict
+// added in ES 5
+// use this for Valina Javascript.
+'use strict';
+
+// 2. Variable
+// let (added in ES6)
+
+let name = 'seungchae';
+console.log(name);
+name = 'hello';
+console.log(name);
