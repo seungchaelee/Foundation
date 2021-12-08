2e6d88aa6815299140ce2f6def8033dd4732040e
seungchae <id6639@naver.com>
2021-12-08 16:36:53 +0900

    3th

--- class_03/Variable.js
+++ class_03/Variable.js
@@ -29,19 +29,23 @@ console.log(age);
 // 3. Constant, r(read only)
 // use const whenever possible.
 // only use let if variable needs to change.
-
-// favor immutable data type always for a few reasons:
-// - security
-// - thread safety
-// - reduce human mistakes
-const daysInweek = 7;
+const daysInWeek = 7;
 const maxNumber = 5;
+
+// Note!
+// Immutable data types: premitive types, frozen objects (i.e object.freeze())
+// Mutable data types: all objects by default are mutable in JS
+// favor immutable data type always for a few reasons:
+//  - security
+//  - thread safety
+//  - reduce human mistakes
 
 // 4. Variable types
 // primitive, single item: number, string, boolean, null, undefiend, symbol
 // object, box container
 // function, first-class function
 
+// number
 const count = 17; // integer
 const size = 17.1;  // decimal number
 console.log(`value: ${count}, type: ${typeof count}`);
