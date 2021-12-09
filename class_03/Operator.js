8f086f6161bbfd344d8b9bc190a876b89584a145
seungchae <id6639@naver.com>
2021-12-09 20:26:47 +0900

    3th

--- class_03/Operator.js
+++ class_03/Operator.js
@@ -1,4 +1,7 @@
 // 1. String concatenation
 console.log('my' + ' cat');
 console.log('1' + 2);
+console.log(2 + '1');
+console.log('1' + '3');
+console.log(1 + 2);
 console.log(`string literals: 1 + 2 = ${1 + 2}`);
