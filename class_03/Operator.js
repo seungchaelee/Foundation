98920b3c3281a81cfddec133845fbaed506e6cb1
seungchae <id6639@naver.com>
2021-12-10 21:00:52 +0900

    3th

--- class_03/Operator.js
+++ class_03/Operator.js
@@ -4,4 +4,16 @@ console.log('1' + 2);
 console.log(2 + '1');
 console.log('1' + '3');
 console.log(1 + 2);
-console.log(`string literals: 1 + 2 = ${1 + 2}`);
+console.log(`string literals:
+....
+1 + 2 = ${1 + 2}`);
+
+console.log("Lee's \n book \t kkk");
+
+// 2. Numeric operators
+console.log(1 + 1); // add
+console.log(1 - 1); // substract
+console.log(1 / 1); // divide
+console.log(1 * 1); // multiply
+console.log(5 % 2); // remainder
+console.log(2 ** 3); // exponentiation
