9da9a082ebb00c31d728fd8bd405198f3ba4a9f9
seungchae <id6639@naver.com>
2021-12-10 23:44:19 +0900

    3th

--- class_03/Operator.js
+++ class_03/Operator.js
@@ -19,6 +19,27 @@ console.log(2 ** 10); // exponentiation
 
 // 3. Increment and decrement operators
 let counter = 1;
+
 const preIncrement = ++counter;
 // counter = counter + 1;
 // preIncrement = counter;
+console.log(`preIncrement: ${preIncrement}, counter: ${counter}`);
+
+const postIncrement = counter++;
+// postIncrement = counter;
+// counter = counter + 1;
+console.log(`postIncrement: ${postIncrement}, counter: ${counter}`);
+
+const preDecrement = --counter;
+console.log(`preDecrement: ${preDecrement}, counter: ${counter}`);
+
+const postDecrement = counter--;
+console.log(`postDecrement: ${postDecrement}, counter: ${counter}`);
+
+// 4. Assignment operators
+let x = 3;
+let y = 6;
+x += y; // x = x + y;
+x -= y;
+x *= y;
+x /= y;
