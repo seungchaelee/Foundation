4df9cc216c0371cf6cdcae41812e600b84c9d7f9
seungchae <id6639@naver.com>
2021-12-15 23:39:46 +0900

    3th

--- class_03/Function.js
+++ class_03/Function.js
@@ -4,8 +4,12 @@
 // - performs a task or calculates a value
 
 // 1. Function declaration
-// function name(param1, param2) { body... return}
+// function name(param1, param2) { body... return; }
 // one function === one thing
-// naming: doSomething, command, verb
+// naming: doSomething, command, verb(동사)
 // e.g. createCardAndPoint -> createCard, createPoint
 // function is object in JS
+function printHello () {
+  console.log('Hello')
+}
+printHello();
