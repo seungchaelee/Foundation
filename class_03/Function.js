a4e3cd665c4599f44529e0e3e7623c255284d177
seungchae <id6639@naver.com>
2021-12-22 00:35:39 +0900

    3th

--- class_03/Function.js
+++ class_03/Function.js
@@ -114,3 +114,12 @@ function randomQuiz(answer, printYes, printNo) {
     printNo();
   }
 }
+const printYes = function () {
+  console.log('yes!');
+};
+
+const printNo = function () {
+  console.log('no!');
+};
+randomQuiz('wrong', printYes, printNo);
+randomQuiz('love u',printYes, printNo);
