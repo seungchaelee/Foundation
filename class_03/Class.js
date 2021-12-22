e4fad9c0d2220825c280e1d3380448b063b2de85
seungchae <id6639@naver.com>
2021-12-22 21:52:27 +0900

    3th

--- class_03/Class.js
+++ class_03/Class.js
@@ -1,5 +1,7 @@
-// 
-// 
-// 
-// 
-// 
+'use strict';
+// Object-oriendted programming
+// class: template
+// object: instance of a class
+// JavaScript classes
+// - introduced in ES6
+// - syntactical sugar over prototype-based inheritance
