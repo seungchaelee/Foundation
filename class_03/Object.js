a9b753dc3b538f5f7525b4f13c820e9c07ba116c
seungchae <id6639@naver.com>
2021-12-29 22:59:07 +0900

    3th

---
+++ class_03/Object.js
@@ -0,0 +1,11 @@
+// Objects
+// one of the JavaScript's data types.
+// a collection of related data and/or functionality.
+// Nearly all objects in JavaScript are instances of Object
+
+const name = 'leesch';
+const age = 25;
+print(name, age);
+function print(name, age) {
+  console.log(name);
+}
