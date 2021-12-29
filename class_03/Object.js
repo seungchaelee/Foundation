35616ada25c06eb709234d1e90a94a78342352f5
seungchae <id6639@naver.com>
2021-12-29 23:03:59 +0900

    3th

--- class_03/Object.js
+++ class_03/Object.js
@@ -3,8 +3,9 @@
 // a collection of related data and/or functionality.
 // Nearly all objects in JavaScript are instances of Object
 
-const obj1 = {};
-const obj2 = new Object();
+const obj1 = {}; // 'object literal' syntax
+const obj2 = new Object(); // 'object constructor' syntax
+
 function print(person) {
   console.log(person.name);
   console.log(person.age);
