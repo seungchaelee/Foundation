2338f3f9faa5bb8b642665ffa6b68930314cecb8
seungchae <id6639@naver.com>
2021-12-30 23:23:13 +0900

    3th

--- class_03/Object.js
+++ class_03/Object.js
@@ -2,7 +2,9 @@
 // one of the JavaScript's data types.
 // a collection of related data and/or functionality.
 // Nearly all objects in JavaScript are instances of Object
+// object = { key: value };
 
+// 1. Listerals and properties
 const obj1 = {}; // 'object literal' syntax
 const obj2 = new Object(); // 'object constructor' syntax
 
@@ -14,5 +16,10 @@ function print(person) {
 const leesch = { name: 'leesch', age: 25 };
 print(leesch);
 
+// with Javascript magic (dynamically typed language)
+// can add properties later
 leesch.hasJob = true;
+console.log(leesch.hasJob);
+
+delete leesch.hasJob;
 console.log(leesch.hasJob);
