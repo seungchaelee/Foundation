f99f2131d3429e7df7f9130ff0cbc302dc39e78a
seungchae <id6639@naver.com>
2022-01-02 18:07:23 +0900

    3th

--- class_03/Object.js
+++ class_03/Object.js
@@ -85,3 +85,13 @@ for (key in user) {
 }
 console.clear();
 console.log(user3);
+
+const user4 = Object.assign({}, user);
+console.log(user4);
+
+// another example
+const fruit1 = { color: 'red' };
+const fruit2 = { color: 'blue', size: 'big' };
+const mixed = Object.assign({}, fruit1, fruit2);
+console.log(mixed.color);
+console.log(mixed.size);
