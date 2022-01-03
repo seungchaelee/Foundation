b79e94079ed9a2350d56c5ed166e9ee5394f4991
seungchae <id6639@naver.com>
2022-01-03 22:05:29 +0900

    3th

--- class_03/Array.js
+++ class_03/Array.js
@@ -9,3 +9,16 @@ const arr2 = [1, 2];
 // 2. Index position
 const fruits = ['🍎', '🍌'];
 console.log(fruits);
+console.log(fruits.length);
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
+console.log(fruits[fruits.length - 1]);
+
+console.clear();
+// 3. Looping over an array
+// print all fruits
+// a. for
+for (let i = 0; i < fruits.length; i++) {
+  console.log(fruits[i]);
+}
