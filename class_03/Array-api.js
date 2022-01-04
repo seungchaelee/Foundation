03a07ecdc51edc5d8e05dd9e1b0859984d21ef65
seungchae <id6639@naver.com>
2022-01-05 00:08:15 +0900

    3th

--- class_03/Array-api.js
+++ class_03/Array-api.js
@@ -17,6 +17,7 @@
   const array = [1, 2, 3, 4, 5];
   const result = array.reverse();
   console.log(result);
+  console.log(array);
 }
 
 // Q4. make new array without the first two elements
@@ -24,4 +25,34 @@
   const array = [1, 2, 3, 4, 5];
   const result = array.slice(2, 5);
   console.log(result);
+  console.log(array);
+}
+
+class Student {
+  constructor(name, age, enrolled, score) {
+    this.name = name;
+    this.age = age;
+    this.enrolled = enrolled;
+    this.score = score;
+  }
+}
+const students = [
+  new Student('A', 29, true, 45),
+  new Student('B', 28, false, 80),
+  new Student('C', 30, true, 90),
+  new Student('D', 40, false, 66),
+  new Student('E', 18, true, 88),
+];
+
+// Q5. find a student with the score 90
+{
+  const result = students.find(student => {
+    return student.score === 90;
+  });
+  console.log(result);
+}
+
+// Q6. make an attay of enrolled students
+{
+
 }
