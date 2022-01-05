f5b026d0fdcace6896b86accb67a437c50def628
seungchae <id6639@naver.com>
2022-01-05 23:30:21 +0900

    3th

--- class_03/Array-api.js
+++ class_03/Array-api.js
@@ -46,9 +46,7 @@ const students = [
 
 // Q5. find a student with the score 90
 {
-  const result = students.find(student => {
-    return student.score === 90;
-  });
+  const result = students.find(student => student.score === 90);
   console.log(result);
 }
 
