d5514e9f1a6f663fffd580fef5e8efee49844cfb
seungchae <id6639@naver.com>
2022-01-12 23:22:45 +0900

    3th

--- class_03/Json.js
+++ class_03/Json.js
@@ -9,5 +9,15 @@ console.log(json);
 json = JSON.stringify(['apple', 'banana']);
 console.log(json);
 
+const rabbit = {
+  name: 'tori',
+  color: 'white',
+  size: null,
+  birthDate: new Date(),
+  jump: () => {
+    console.log(`${name} can jump!`);
+  },
+};
+
 // 2. JSON to Object
 // parse(json)
