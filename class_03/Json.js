6930585600ed1e8e9c9f46bcd55aba3c096e3839
seungchae <id6639@naver.com>
2022-01-12 21:59:23 +0900

    3th

--- class_03/Json.js
+++ class_03/Json.js
@@ -3,5 +3,11 @@
 
 // 1. Object to JSON
 // stringify(obj)
+let json = JSON.stringify(true);
+console.log(json);
 
-// 2. JSON
+json = JSON.stringify(['apple', 'banana']);
+console.log(json);
+
+// 2. JSON to Object
+// parse(json)
